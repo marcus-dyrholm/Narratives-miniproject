314d7798b21e66d0aac7b005f907f6538e6ab303
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2022-05-02 17:29:57 +0200

    Did stuff

---
+++ New Unity Project/Assets/Narratives Script/ObjCollisionDetection.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjCollisionDetection : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Color _savedColor;
+
+    private void Start()
+    {
+        _renderer = this.GetComponent<Renderer>();
+        _savedColor = _renderer.material.GetColor("_TintColor");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Object")
+        {
+            _renderer.material.SetColor("_TintColor", Color.magenta);
+            Debug.Log("aslmdadm");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _renderer.material.SetColor("_TintColor", _savedColor);
+    }
+}
