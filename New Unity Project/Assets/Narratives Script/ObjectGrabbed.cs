7a7e9ec319e5ed9b104f84ee38870b76c78e49cd
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2022-05-16 17:50:16 +0200

    12345

--- New Unity Project/Assets/Narratives Script/ObjectGrabbed.cs
+++ New Unity Project/Assets/Narratives Script/ObjectGrabbed.cs
@@ -4,15 +4,34 @@ using System.Collections.Generic;
 using Oculus.Interaction;
 using Oculus.Interaction.HandPosing;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ObjectGrabbed : MonoBehaviour
 {
     public OVRGrabbable grabbable;
     
-    public ObjCollisionDetection collisionDetection1;
-    public ObjCollisionDetection collisionDetection2;
+    public ObjCollisionDetection RightCollider;
+    public ObjCollisionDetection LeftCollider;
+
+    public VideoClip RightSideVideo;
+    public VideoClip LeftSideVideo;
+
+    private bool lerpToVideo;
+
+    public Material skybox;
 
     public Animator anim;
+
+    public VideoPlayer videoPlayer;
+
+    private float blendValue;
+    private float blendMin = 0;
+    private float blendMax = 1.0f;
+
+    private void Start()
+    {
+        skybox = RenderSettings.skybox;
+    }
 
     private void Update()
     {
@@ -24,6 +43,39 @@ public class ObjectGrabbed : MonoBehaviour
         {
             OnRelese();
         }
+
+        if (RightCollider.isColliding)
+        {
+            ChangeVideo(RightSideVideo);
+            Debug.Log("is colliding");
+        }
+        else if(LeftCollider.isColliding)
+        {
+             ChangeVideo(LeftSideVideo);
+        }
+
+        if (!LeftCollider.isColliding && !RightCollider.isColliding)
+        {
+            lerpToVideo = false;
+        }
+
+        if (lerpToVideo)
+        {
+            if (blendValue < blendMax)
+            {
+                blendValue  = blendValue + 1 * Time.deltaTime;
+            }
+            skybox.SetFloat("_Blend", blendValue);
+        }
+        else
+        {
+            if (blendValue > blendMin)
+            {
+                blendValue = blendValue - 1 * Time.deltaTime;
+            }
+            skybox.SetFloat("_Blend", blendValue);
+        }
+        Debug.Log(lerpToVideo);
     }
 
     void OnGrab()
@@ -33,10 +85,15 @@ public class ObjectGrabbed : MonoBehaviour
 
     void OnRelese()
     {
-        if (!collisionDetection1.isColliding && !collisionDetection2.isColliding)
+        if (!RightCollider.isColliding && !LeftCollider.isColliding)
         {
             anim.SetBool("Start", false);
-            anim.SetFloat("Speed", -1);
         }
+    }
+
+    void ChangeVideo(VideoClip video)
+    {
+        videoPlayer.clip = video;
+        lerpToVideo = true;
     }
 }
